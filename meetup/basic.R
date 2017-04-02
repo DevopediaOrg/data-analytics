63ef3e821c0b8c699962ab4481b4ad6e2603943f
Arvind Padmanabhan <arvindpdmn@gmail.com>
2017-04-02 18:25:26 +0530

    Use overview data when individual page downloads have some data missing.

--- meetup/basic.R
+++ meetup/basic.R
@@ -3,40 +3,43 @@ library(ggplot2)
 library(grid)
 library(XML)
 
-getGroupUrls <- function() {
+getOverviewData <- function() {
     # Source: https://www.meetup.com/find/tech/?allMeetups=false&radius=2&userFreeform=Bangalore%2C+India&mcId=z1018093&mcName=Bangalore%2C+IN&sort=recommended&eventFilter=mysugg
     # Filtered to topic Tech
     # Copied manually from web browser and saved into HTML file
 
     html <- readLines('BangaloreTechMeetups.htm')
     doc = htmlParse(html, asText=TRUE)
-    groupUrls <- xpathSApply(doc, "//li[@class='groupCard tileGrid-tile']/div/a[@itemprop='url']", xmlGetAttr, 'href')
-    #groupUrls <- unique.default(groupUrls)
+    urls <- xpathSApply(doc, "//li[@class='groupCard tileGrid-tile']/div/a[@itemprop='url']", xmlGetAttr, 'href')
+    numMembers <- sub("^\\s*.*We're\\s*([\\d,]+)\\s*(.*)\\s*$", "\\1~\\2", xpathSApply(doc, "//p[@class='small ellipsize']", xmlValue), perl=T)
+    salutations <- sub(".*~", "", numMembers)
+    numMembers <- sub("~.*", "\\1", numMembers)
+    numMembers <- sub(",", "", numMembers)
 
-    #numMembers <- sub("^\\s*.*We're\\s*([\\d,]+)\\s*(.*)\\s*$", "\\1~\\2", xpathSApply(doc, "//p[@class='small ellipsize']", xmlValue), perl=T)
-    #salutations <- sub(".*~", "", numMembers)
-    #numMembers <- sub("~.*", "\\1", numMembers)
-    #numMembers <- sub(",", "", numMembers)
+    od <- data.table(cbind(urls, numMembers, salutations))
+    od <- od[!duplicated(od$urls)]
+    od <- paste(od$urls,od$numMembers,od$salutations,sep='~')
 }
 
-downloadGroupData <- function(url) {
-    fname <- paste0(sub(".*/([^/]+)/$", "\\1", url, perl=TRUE), '.htm')
+downloadGroupData <- function(od) {
+    od <- strsplit(od, '~')[[1]] # url, members, salutation
+    fname <- paste0(sub(".*/([^/]+)/$", "\\1", od[1], perl=TRUE), '.htm')
     if (!file.exists(fname))
-        download.file(url, fname, 'curl', quiet=T)
+        download.file(od[1], fname, 'curl', quiet=T)
 
     html <- readLines(fname)
     doc = htmlParse(html, asText=TRUE)
 
-    name <- xpathSApply(doc, "//div[@id='chapter-banner']/h1/a/span", xmlValue) 
+    name <- xpathSApply(doc, "//div[@id='chapter-banner']/h1/a/span", xmlValue)
     foundingDate <- as.Date(xpathSApply(doc, "//span[@itemprop='foundingDate']", xmlValue), format = "%Y%m%d")
 
     salutation <- xpathSApply(doc, "//span[@class='unit size5of7 wrapNice']", xmlValue)
-    if (length(salutation) == 0) salutation <- 'Members'
+    if (length(salutation) == 0) salutation <- od[3]
     
     fields <- c('numOfMembers', xpathSApply(doc, "//span[@class='unit size5of7']", xmlValue))
     values <- xpathSApply(doc, "//span[@class='lastUnit align-right']", xmlValue)
     if (length(values)!=length(fields)) {
-        values <- c("0", values) # zero members: update later
+        values <- c(od[2], values) # take member data from overview
     }
 
     df <- data.frame(t(values))
@@ -46,10 +49,10 @@ downloadGroupData <- function(url) {
     if (is.null(df$`Group reviews`)) df$`Group reviews`<- factor(0)
     if (is.null(df$`Upcoming Meetups`)) df$`Upcoming Meetups`<- factor(0)
     if (is.null(df$`Past Meetups`)) df$`Past Meetups`<- factor(0)
-    
+
     grpdata <- c(df$numOfMembers,
                  levels(df$`Group reviews`), levels(df$`Upcoming Meetups`), levels(df$`Past Meetups`),
-                 url, fname, name, as.character(foundingDate), salutation)
+                 od[1], fname, name, as.character(foundingDate), salutation)
 
     return(grpdata)
 }
@@ -98,7 +101,7 @@ basicNumbers <- function(d) {
         ggplot(n, aes(x=X, y=Y)) +
         geom_point(data=n, aes(x=X, y=Y, size=100, colour=Colour), alpha=.5, show.legend=F) +
         ggtitle(paste("Some Basic Numbers.", 
-                      "\nData Source: Meetup.com, 30 Mar 2017.", sep='')) +
+                      "\nData Source: Meetup.com, 02 Apr 2017.", sep='')) +
         commonTheme() +
         theme(panel.grid.major.y = element_blank(),
               axis.title.x=element_blank(),
@@ -124,7 +127,7 @@ mostPopular <- function(d, topn) {
         ggplot(n, aes(x=X, y=Y)) +
         geom_point(data=n, aes(x=X, y=Y, size=100, colour=Colour), alpha=.5, show.legend=F) +
         ggtitle(paste(topn[2]," Most Popular (", topn[1], "+ members) Bangalore Tech Groups.", 
-                      "\nData Source: Meetup.com, 30 Mar 2017.", sep='')) +
+                      "\nData Source: Meetup.com, 02 Apr 2017.", sep='')) +
         commonTheme() +
         theme(panel.grid.major.y = element_blank(),
               axis.title.x=element_blank(),
@@ -148,7 +151,7 @@ membersPerTopGroups <- function(d, cutoff) {
         ggplot(data=top, aes(x=name,y=members)) +
         geom_bar(position="dodge", stat="identity", width=0.5, fill='steelblue1') +
         ggtitle(paste("Number of Members Per Group: Only Popular Bangalore Tech Groups (3000+ Members). ", 
-                      "Count: ", numToppers, ".\nData Source: Meetup.com, 30 Mar 2017.", sep='')) +
+                      "Count: ", numToppers, ".\nData Source: Meetup.com, 02 Apr 2017.", sep='')) +
         coord_flip() +
         commonTheme() +
         theme(panel.grid.major.x = element_line(colour = "#eeeeee")) +
@@ -174,7 +177,7 @@ membershipHistogram <- function(d, bins, fname) {
         commonTheme() +
         scale_x_continuous(breaks=bins) +
         ggtitle(paste("Histogram of Bangalore Tech Groups by Size [",bins[1],", ",last(bins),"]", 
-                      "\nData Source: Meetup.com, 30 Mar 2017.", sep='')) +
+                      "\nData Source: Meetup.com, 02 Apr 2017.", sep='')) +
         xlab("Number of Members") +
         ylab("Number of Groups") +
         geom_text(data=countGrps[cnts>0], aes(label=paste0(percent,"%"), hjust=0.5, vjust=-0.2), color="#aaaaaa", size=3)
@@ -187,7 +190,7 @@ topTopicsHistogram <- function(top) {
         ggplot(data=top, aes(x=topic,y=members)) +
         geom_bar(position="dodge", stat="identity", width=0.5, fill='steelblue1') +
         ggtitle(paste("Number of Bangalore Tech Groups For Popular Topics.", 
-                      "\nData Source: Meetup.com, 30 Mar 2017.", sep='')) +
+                      "\nData Source: Meetup.com, 02 Apr 2017.", sep='')) +
         coord_flip() +
         commonTheme() +
         theme(panel.grid.major.x = element_line(colour = "#eeeeee")) +
@@ -199,8 +202,8 @@ topTopicsHistogram <- function(top) {
 }
 
 main <- function() {
-    urls <- getGroupUrls()
-    all <- data.table(t(sapply(urls, downloadGroupData, USE.NAMES=F)))
+    od <- getOverviewData()
+    all <- data.table(t(sapply(od, downloadGroupData, USE.NAMES=F)))
     colnames(all) <- c('members', 'reviews', 'upcoming', 'past', 'url', 'fname', 'name', 'created', 'salutation')
     all <- all[, c(lapply(.(members,reviews,upcoming,past), as.integer), .(url, fname, name, created, salutation))]
     colnames(all) <- c('members', 'reviews', 'upcoming', 'past', 'url', 'fname', 'name', 'created', 'salutation')
